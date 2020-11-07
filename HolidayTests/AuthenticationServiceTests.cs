933b4e1f35ccb69ff426e8e47daba1fe9259b23c
hjonatha <hu.jonathan@asml.com>
2020-11-07 14:01:01 +0800

    refactor to use Nsbstiute

--- HolidayTests/AuthenticationServiceTests.cs
+++ HolidayTests/AuthenticationServiceTests.cs
@@ -1,25 +1,27 @@
 ﻿using Lib;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace HolidayTests
 {
     [TestFixture]
-    public class AuthenticationServiceTests : IToken
+    public class AuthenticationServiceTests
     {
         [Test()]
         public void is_valid()
         {
-            var target = new AuthenticationService(new ProfileDao(), this);
+            var profile = Substitute.For<IProfile>();
+            profile.GetPassword("joey").Returns("91");
+
+            var token = Substitute.For<IToken>();
+            token.GetRandom("").ReturnsForAnyArgs("000000");
+
+            var target = new AuthenticationService(profile, token);
 
             var actual = target.IsValid("joey", "91000000");
 
             //always failed
             Assert.IsTrue(actual);
-        }
-
-        public string GetRandom(string account)
-        {
-            return "000000";
         }
     }
 }
