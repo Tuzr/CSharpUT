2cabf43b6170c48aa950327a8422e06579e81550
hjonatha <hu.jonathan@asml.com>
2020-11-07 12:33:35 +0800

    finish authentication service test

--- HolidayTests/AuthenticationServiceTests.cs
+++ HolidayTests/AuthenticationServiceTests.cs
@@ -4,17 +4,22 @@ using NUnit.Framework;
 namespace HolidayTests
 {
     [TestFixture]
-    public class AuthenticationServiceTests
+    public class AuthenticationServiceTests : IToken
     {
         [Test()]
         public void is_valid()
         {
-            var target = new AuthenticationService();
+            var target = new AuthenticationService(new ProfileDao(), this);
 
             var actual = target.IsValid("joey", "91000000");
 
             //always failed
             Assert.IsTrue(actual);
-        } 
+        }
+
+        public string GetRandom(string account)
+        {
+            return "000000";
+        }
     }
 }
